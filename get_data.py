157d1cb1b46f1dd2872301a84deecd73179f2b05
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2023-12-12 00:53:51 -0800

    Added a function get_prime_part_image() to enable you to download images of prime items to get_data.py

--- get_data.py
+++ get_data.py
@@ -1,6 +1,57 @@
 
 import psycopg2
 import requests
+import urllib
+import fandom
+from bs4 import BeautifulSoup
+
+
+def get_prime_part_image(item_to_find):
+
+    possible_image=[]
+    global t, ImageToDownload
+    fandom.set_wiki('warframe')
+
+    # Search for a page (Mirage Prime in this case)
+    search_results = fandom.search(item_to_find, results=1)
+
+    # Check if there are search results
+    if search_results:
+        # Get the page ID from the search results
+        page_id = search_results[0][1]
+
+        # Create a FandomPage object
+        fandom_page = fandom.FandomPage(wiki='warframe', language='en', pageid=page_id)
+
+        # Create a BeautifulSoup object using the HTML content of the page
+        soup = BeautifulSoup(fandom_page.html, 'html.parser')
+
+        # Find all image tags in the HTML
+        images = soup.find_all('img')
+
+        # Extract and print the image URLs
+        for img in images:
+            src = img.get('src')
+            if src:
+                if f"{item_to_find.replace(" ","")}" in src:
+                    possible_image.append(src)
+        print(possible_image)
+        if len(possible_image)>1:
+            ImageToDownload=possible_image[1]
+
+        if len(possible_image)<=1:
+            ImageToDownload=possible_image[0]
+
+
+
+    else:
+        print("No search results found for 'Mirage Prime'.")
+
+    urllib.request.urlretrieve(ImageToDownload, "image.png")
+
+
+
+
 
 def get_available_prime():
     current_prime_part=set()
@@ -56,46 +107,4 @@ def get_prime_part_relic(item_to_find):
 
     return item_list
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#cursor.execute(f"""INSERT INTO warframe_relic(relic_name) VALUES('{relic_info}')""")
-        #connection.commit()
-
-'''
-# Create a cursor
-
-cursor.execute("""SELECT * 
-FROM Warframe_relic""")
-print(cursor.fetchone())
-
-# Now you can use the 'cursor' object to execute SQL queries
-
-# Remember to close the cursor and connection when you're done
-
-'''
+#get_prime_part_image('Octavia Prime')
